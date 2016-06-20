b93b31b0ee69d112c197b723812d94eda81d3111
josuecadillo <jfcadillo951@gmail.com>
2016-06-20 01:35:58 -0500

    first commit

---
+++ Example/Pods/Target Support Files/ThirdPapayaLabel/ThirdPapayaLabel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ThirdPapayaLabel : NSObject
+@end
+@implementation PodsDummy_ThirdPapayaLabel
+@end
---
+++ Example/Pods/Target Support Files/ThirdPapayaLabel/ThirdPapayaLabel-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "CustomLabel.h"
+
+FOUNDATION_EXPORT double ThirdPapayaLabelVersionNumber;
+FOUNDATION_EXPORT const unsigned char ThirdPapayaLabelVersionString[];
+
